7431bcab7279e244d91b3735e43c7e40373d6b11
yuezhilunhui2009 <yuezhilunhui2009@gmail.com>
2018-09-27 01:03:47 +0800

    feat: 完成 position 示例;

--- pages/styles-demo/position/position.js
+++ pages/styles-demo/position/position.js
@@ -1,11 +1,42 @@
 // pages/styles-demo/position/position.js
+const domNameAndDataMap = {
+  ['eg-position-1']: 'egPosition1',
+  ['eg-position-2']: 'egPosition2',
+  ['eg-top-1']: 'egTop1',
+  ['eg-top-2']: 'egTop2',
+  ['eg-right-1']: 'egRight1',
+  ['eg-right-2']: 'egRight1',
+  ['eg-bottom-1']: 'egBottom1',
+  ['eg-bottom-2']: 'egBottom2',
+  ['eg-left-1']: 'egLeft1',
+  ['eg-left-2']: 'egLeft2',
+}
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    position: [
+      {name: 'static', value: 'static', checked: 'checked'},
+      {name: 'relative', value: 'relative'},
+      {name: 'absolute', value: 'absolute'},
+      {name: 'fixed', value: 'fixed'},
+      {name: 'center', value: 'center'},
+      {name: 'page', value: 'page'},
+      {name: 'sticky', value: 'sticky'},
+    ],
+    egPosition1: 'static',
+    egPosition2: 'static',
+    egTop1: '0',
+    egTop2: '0',
+    egRight1: '0',
+    egRight2: '0',
+    egBottom1: '0',
+    egBottom2: '0',
+    egLeft1: '0',
+    egLeft2: '0',
   },
 
   /**
@@ -62,5 +93,19 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  onRadioChange: function(e) {
+    let { value } = e.detail
+    this.setData({
+      [domNameAndDataMap[e.currentTarget.dataset.egname]]: e.detail.value,
+    })
+  },
+
+  onSlideChanging: function (e) {
+    let { value } = e.detail
+    this.setData({
+      [domNameAndDataMap[e.currentTarget.dataset.egname]]: e.detail.value,
+    })
   }
 })
